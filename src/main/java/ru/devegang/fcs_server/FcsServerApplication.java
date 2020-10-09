f68d1010d8275f4be7b6e5e78a06796d5e4aa74e
AlNualD <nuald@outlook.com>
2020-10-09 09:30:29 +0300

    FCS-7 Initial commit

---
+++ src/main/java/ru/devegang/fcs_server/FcsServerApplication.java
@@ -0,0 +1,13 @@
+package ru.devegang.fcs_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/devegang/fcs_server/FcsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.devegang.fcs_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
